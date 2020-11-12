7808fcb1ca0b37d82c1fc540663658f912f86f70
murilo menezes <murilolcm@al.insper.edu.br>
2020-11-12 08:33:53 -0300

    git revert

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
+
+
 module.exports = (server) => {
 
- const router = express.Router()
- server.use(router)
- router.get('/', (req, res, next) => res.status(200).json({message:
-"API funcionando /o/"}));
+
+
+	const router = express.Router()
+	server.use(router)
+	router.get('/', (req, res, next) => res.status(200).json({message: "API FUNCIONANDO /o/"}))
+	
 }
